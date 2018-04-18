8e7da7d1076a9a1ec6d98065a3afc36fb37af59a
Peter Wu <petey888@gmail.com>
2018-04-18 16:49:28 -0700

    Initial button UI layout

---
+++ Set/Set.swift
@@ -0,0 +1,23 @@
+//
+//  Set.swift
+//  Set
+//
+//  Created by Peter Wu on 4/17/18.
+//  Copyright © 2018 Zero. All rights reserved.
+//
+
+import Foundation
+
+class Set {
+    
+    /*
+     
+     Four features:
+     1. Symbols - "▲", "◼︎", and "✦".
+     2. Numbers - one, two, or three symbols. "▲", "▲▲", and "▲▲▲"
+     3. Shading - solid, striped, or open
+     4. Color - red, green or purple
+ 
+     */
+    
+}
--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let symbol1 = "▲"
+        let symbol2 = "◼︎"
+        let symbol3 = "✦"
+
+        // filled solid color
+        let attributeSolidFilled: [NSAttributedStringKey: Any] = [
+            NSAttributedStringKey.foregroundColor: UIColor.blue
+        ]
+        // "striped" color
+        let attributeStripedFilled: [NSAttributedStringKey: Any] = [
+            NSAttributedStringKey.foregroundColor: UIColor.red.withAlphaComponent(0.15)
+        ]
+        
+        let attributeOutlined: [NSAttributedStringKey: Any] = [
+            NSAttributedStringKey.foregroundColor: UIColor.red,
+            NSAttributedStringKey.strokeColor: UIColor.black,
+            NSAttributedStringKey.strokeWidth: -3.0
+        ]
+        let symbolAttributed = NSAttributedString(string: symbol3, attributes: attributeStripedFilled)
+        
     }
 
     override func didReceiveMemoryWarning() {
